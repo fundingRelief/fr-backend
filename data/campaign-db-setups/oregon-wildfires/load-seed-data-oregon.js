93cf893729dd0ae51d5b2291761b476552de5f1e
Michael Romay <amerror@gmail.com>
2020-10-13 13:03:42 -0700

    cleaned up load-seed-data-oregon

--- data/campaign-db-setups/oregon-wildfires/load-seed-data-oregon.js
+++ data/campaign-db-setups/oregon-wildfires/load-seed-data-oregon.js
@@ -1,6 +1,4 @@
 const client = require('../../../lib/client');
-// const data = require('./data.json');
-
 const { scrapeThing } = require('../../../lib/goFundMeScrape/gfmData');
 
 run();
@@ -10,18 +8,6 @@ async function run() {
     const data = await scrapeThing(
       'https://www.gofundme.com/c/act/oregon-fires'
     );
-    // const data2 = await scrapeThing(
-    //   'https://www.gofundme.com/c/act/northern-california-fires'
-    // );
-    // const data3 = await scrapeThing(
-    //   'https://www.gofundme.com/c/act/southern-california-fires'
-    // );
-    // const data4 = await scrapeThing(
-    //   'https://www.gofundme.com/c/act/central-california-fires'
-    // );
-    // const data5 = await scrapeThing(
-    //   'https://www.gofundme.com/c/act/washington-fires'
-    // );
 
     await client.connect();
     // console.log(data);
@@ -48,98 +34,6 @@ async function run() {
         );
       })
     );
-
-    // await Promise.all(
-    //   data2.map((campaign) => {
-    //     return client.query(
-    //       `
-    //                 INSERT INTO campaigns (campaign_name, current_amount, goal, percentage_raised, location, img_url, link_url, description, last_donation, cause)
-    //                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
-    //             `,
-    //       [
-    //         campaign.campaign_name,
-    //         campaign.current_amount,
-    //         campaign.goal,
-    //         campaign.percentage_raised,
-    //         campaign.location,
-    //         campaign.img_url,
-    //         campaign.link_url,
-    //         campaign.description,
-    //         campaign.last_donation,
-    //         campaign.cause,
-    //       ]
-    //     );
-    //   })
-    // );
-
-    // await Promise.all(
-    //   data3.map((campaign) => {
-    //     return client.query(
-    //       `
-    //                 INSERT INTO campaigns (campaign_name, current_amount, goal, percentage_raised, location, img_url, link_url, description, last_donation, cause)
-    //                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
-    //             `,
-    //       [
-    //         campaign.campaign_name,
-    //         campaign.current_amount,
-    //         campaign.goal,
-    //         campaign.percentage_raised,
-    //         campaign.location,
-    //         campaign.img_url,
-    //         campaign.link_url,
-    //         campaign.description,
-    //         campaign.last_donation,
-    //         campaign.cause,
-    //       ]
-    //     );
-    //   })
-    // );
-
-    // await Promise.all(
-    //   data4.map((campaign) => {
-    //     return client.query(
-    //       `
-    //                 INSERT INTO campaigns (campaign_name, current_amount, goal, percentage_raised, location, img_url, link_url, description, last_donation, cause)
-    //                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
-    //             `,
-    //       [
-    //         campaign.campaign_name,
-    //         campaign.current_amount,
-    //         campaign.goal,
-    //         campaign.percentage_raised,
-    //         campaign.location,
-    //         campaign.img_url,
-    //         campaign.link_url,
-    //         campaign.description,
-    //         campaign.last_donation,
-    //         campaign.cause,
-    //       ]
-    //     );
-    //   })
-    // );
-
-    // await Promise.all(
-    //   data5.map((campaign) => {
-    //     return client.query(
-    //       `
-    //                 INSERT INTO campaigns (campaign_name, current_amount, goal, percentage_raised, location, img_url, link_url, description, last_donation, cause)
-    //                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
-    //             `,
-    //       [
-    //         campaign.campaign_name,
-    //         campaign.current_amount,
-    //         campaign.goal,
-    //         campaign.percentage_raised,
-    //         campaign.location,
-    //         campaign.img_url,
-    //         campaign.link_url,
-    //         campaign.description,
-    //         campaign.last_donation,
-    //         campaign.cause,
-    //       ]
-    //     );
-    //   })
-    // );
 
     console.log('seed data load complete');
   } catch (err) {
